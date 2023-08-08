6145111f68074429d17aa444d72529c1402bb606
Taha Kaçmaz <hi@noir.land>
2023-08-08 16:16:53 +0300

    init

---
+++ src/modules/api/api.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ApiService } from './api.service';
+import { JwtGuard } from '@/core/guards/jwt.guard';
+import { AdminGuard } from '@/core/guards/admin.guard';
+import { Throttle } from '@nestjs/throttler';
+
+@Throttle(7, 10 * 60)
+@UseGuards(JwtGuard)
+@Controller('api')
+export class ApiController {
+
+    constructor(private readonly ApiService: ApiService) { }
+
+    @Get()
+    async get() {
+
+        return this.ApiService.get();
+    }
+
+    @UseGuards(AdminGuard)
+    @Get("/admin")
+    async getAdmin() {
+            
+        return this.ApiService.getAdmin();
+        }
+
+}
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { FindDTO } from './etc/find.dto';
+import { RegisterDTO } from './etc/register.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Throttle } from '@nestjs/throttler';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly service: UserService) {}
+
+
+  @Throttle(7, 10 * 60)
+  @Post('/create')
+  async create(@Body() body: RegisterDTO) {
+    return await this.service.create(body);
+  }
+
+  @Post('/login')
+  async login(@Body() body: FindDTO) {
+    let user = await this.service.login(body);
+
+    return user;
+  }
+}
